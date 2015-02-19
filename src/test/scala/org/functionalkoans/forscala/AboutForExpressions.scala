51f92261497c96d14a63ab60624fe767d105e41e
Volodymyr Mandrychenko <vmandrychenko@gmail.com>
2015-02-18 21:30:38 -0500

    137 passing

--- src/test/scala/org/functionalkoans/forscala/AboutForExpressions.scala
+++ src/test/scala/org/functionalkoans/forscala/AboutForExpressions.scala
@@ -10,7 +10,7 @@ class AboutForExpressions extends KoanSuite {
     for (i <- someNumbers)
       sum += i
 
-    sum should equal(__)
+    sum should equal(45)
   }
 
   koan("For loops can contain additional logic") {
@@ -20,7 +20,7 @@ class AboutForExpressions extends KoanSuite {
     for (i <- someNumbers)
       if (i % 2 == 0) sum += i
 
-    sum should equal(__)
+    sum should equal(20)
   }
   
   koan("For expressions can nest, with later generators varying more rapidly than earlier ones") {
@@ -28,7 +28,8 @@ class AboutForExpressions extends KoanSuite {
     val yValues = Range(1, 3)
     val coordinates = for {
       x <- xValues
-      y <- yValues} yield (x, y)
-    coordinates(4) should be(__, __)
+      y <- yValues}
+    yield (x, y)
+    coordinates(4) should be(3, 1)
   }
 }
--- src/test/scala/org/functionalkoans/forscala/AboutInfixTypes.scala
+++ src/test/scala/org/functionalkoans/forscala/AboutInfixTypes.scala
@@ -6,7 +6,7 @@ import support.KoanSuite
 class AboutInfixTypes extends KoanSuite with ShouldMatchers {
 
   koan("""We can make a type infix, meaning that the type can be displayed in complement
-           between two types in order to make a readable delaration""") {
+           between two types in order to make a readable declaration""") {
     case class Person(name: String)
     class Loves[A, B](val a: A, val b: B)
 
@@ -18,7 +18,7 @@ class AboutInfixTypes extends KoanSuite with ShouldMatchers {
     val romeo = new Person("Romeo")
     val juliet = new Person("Juliet")
 
-    announceCouple(new Loves(romeo, juliet)) should be(__)
+    announceCouple(new Loves(romeo, juliet)) should be("Romeo is in love with Juliet")
   }
 
   koan("""Of course we can make this a bit more elegant by creating an infix operator
@@ -38,7 +38,7 @@ class AboutInfixTypes extends KoanSuite with ShouldMatchers {
     val romeo = new Person("Romeo")
     val juliet = new Person("Juliet")
 
-    announceCouple(romeo loves juliet) should be(__)
+    announceCouple(romeo loves juliet) should be("Romeo is in love with Juliet")
   }
 
 
